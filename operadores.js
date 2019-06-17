92b7d8485dc0a29767306c751a00f1f7a093e9bf
Decodificar <jade.denice@outlook.com>
2019-06-17 10:33:40 -0300

    sobre operadores aritmeticos 17.06

---
+++ operadores.js
@@ -0,0 +1,62 @@
+// adição
+2 + 2 // = 4
+
+//subtração
+2 - 2 // = 0
+
+//multiplicação 
+2*2 // = 4
+
+// divisão
+4/2 // = 2
+
+// resto da divisão
+5 % 4 // = 1
+
+//incremento
+/*Adiciona um a variável. Se utilizado antes(++x) adiciona um e retorna o valor, 
+caso o operador seja utilizado depois da variável(x++) retorna o valor e adiciona um.*/
+
+let x = 1
+++x // = 2
+x++ // = 2
+
+//decremento
+/*O comportamento desse operador é parecido com o de incremento(acho que você já entendeu).
+Ele subtrai um da variável. Se utilizado antes(--x) subtrai um e retorna o valor, 
+caso o operador seja utilizado depois da variável(x--) retorna o valor e subtrai um.*/
+
+let x = 2
+--x // = 1
+x-- // = 1
+
+// == comparação
+
+1 == '1' // true
+
+// não igual !=
+
+2 != 3 // TRUE  
+
+// igual restrito ===
+/*Esse operador é mais severo, só retorna verdadeiro se o valor e o tipo comparados forem iguais.
+*/
+3 === 3 // TRUE
+3 === '3' // FALSE
+
+// não igual restrito
+/*Não se engane, esse operador vai retornar verdadeiro se os valores e ou os tipos forem diferentes
+*/
+
+3!=='3' //true
+3!==3 //false
+3!==4 //true
+
+//maior que
+
+1 > 2 //false
+
+
+
+
+
