34a53a38a002441c623b3c3a3387e76f63a0028e
Decodificar <jade.denice@outlook.com>
2019-06-17 11:09:48 -0300

    funções 17.06

---
+++ js/arquivo.js
@@ -0,0 +1,11 @@
+var nome = prompt('Digite seu nome: ');
+alert(nome + ', seja bem vinde');    
+
+let anoAtual = 2019;
+
+let nascimento = prompt("Diga o ano do seu nascimento: ");
+
+let idade = anoAtual - nascimento;
+
+alert("Sua idade é: " + idade + " anos")
+
---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+function bar (){
+    alert('Hello World');
+}
+
+function somar(num1, num2){
+    num1 + num2;
+}
+
+bar();
+alert(somar(3, 5));
+document.getElementById('resultado').innerHTML = somar(1, 2)
