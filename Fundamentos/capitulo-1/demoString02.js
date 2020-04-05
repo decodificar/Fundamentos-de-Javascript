0c913648d5ef5e486debd8658efd16dc2344491c
Jade Denice <45975240+decodificar@users.noreply.github.com>
2020-04-04 23:33:48 -0300

    04.04.20

---
+++ Fundamentos/capitulo-1/demoString02.js
@@ -0,0 +1,14 @@
+    let salarioAmigo = 1000;
+    let meuSalario = '2000';
+    let meuSalarioCorrigido = number(meuSalario);
+
+    console.log(salarioAmigo + meuSalario)
+
+    console.log(typeof(salarioAmigo), salarioAmigo)
+
+    console.log(isNaN(meuSalario))
+
+    let minhaString = 'Olá, mundo';
+    let minhaOutraString = 'Olá mundo2';
+
+    let minhasStringVariaveis = `${minhaString} - ${minhaOutraString}`
---
+++ Fundamentos/capitulo-1/demoVariaveis01.js
@@ -0,0 +1,11 @@
+let nome = "Quake";
+let idade = '31';
+let poder = 'qual é o poder da quake meu deus';
+
+nome = 'Rainbow Brite';
+idade = '19';
+poder = 'Solar light manipulation';
+
+
+
+console.log(`nome: ${nome}, idade: ${idade}, poder: ${poder}`)
